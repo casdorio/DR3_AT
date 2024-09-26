bddc43477b2d215a71551438a25e112e146080d8
casdorio <casdorio@gmail.com>
2024-09-26 12:21:16 -0300

    ajustes

--- eureka-server/src/test/java/com/carlos/infnet/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/carlos/infnet/eureka_server/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.carlos.infnet.eureka;
+package com.carlos.infnet.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
--- status-service/src/main/java/com/carlos/infnet/status_service/controllers/StatusController.java
+++ status-service/src/main/java/com/carlos/infnet/status_service/controllers/StatusController.java
@@ -1,16 +1,13 @@
 package com.carlos.infnet.status_service.controllers;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/status")
 public class StatusController {
 
-    @GetMapping
-    public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Serviço ativo.");
+    @GetMapping("/status")
+    public String getStatus() {
+        return "Service subiu e esta ativo!";
     }
 }
