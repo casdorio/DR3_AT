2da4b93d2272b4357fec2e3a19bec5c8081528dc
casdorio <casdorio@gmail.com>
2024-09-26 11:57:23 -0300

    start

---
+++ status-service/src/main/java/com/carlos/infnet/status_service/StatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.infnet.status_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusServiceApplication.class, args);
+	}
+
+}
